f4ffbd971a3a880d7c630d31bfbbfd6bc170cb87
jonwalker <jonathan.b.walker0@gmail.com>
2015-09-27 15:23:33 -0500

    highlight child comments

--- content.js
+++ content.js
@@ -1,11 +1,13 @@
-/* Listen for messages */
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    /* If the received message has the expected format... */
-    console.log("Hello from CONTENT.JS your grace")
 
-    function getParentCommentNodeList() {
-        var comments = document.querySelectorAll('[class^="entry unvoted"]' || '[class*="entry likes"]' || 'class*="entry dislikes"]');
-        console.log("comment node list", comments)
-        return comments
-    }
+var parentComments = getParentCommentNodeList()
+
+Array.prototype.forEach.call(parentComments, function(comment) {
+    //console.log("Painting it red")
+    comment.style.backgroundColor = "#E8F0FF"
 });
+
+function getParentCommentNodeList() {
+    var comments = document.querySelectorAll('[class^="child"]');
+    console.log("comment node list", comments)
+    return comments
+}
