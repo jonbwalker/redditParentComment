3f0976e9ab4ff082bb9dfffefee0b9b5e0588d96
jonwalker <jonathan.b.walker0@gmail.com>
2015-09-27 11:09:01 -0500

    remove background js

--- content.js
+++ content.js
@@ -8,12 +8,4 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
         console.log("comment node list", comments)
         return comments
     }
-
-
-    if (msg.text && (msg.text == "report_back")) {
-        console.log("HAI FROM CONTENT",msg)
-        /* Call the specified callback, passing 
-         the web-pages DOM content as argument */
-        sendResponse(document.all[0].outerHTML);
-    }
 });
