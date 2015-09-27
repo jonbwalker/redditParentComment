e294e48d5ce5d5f9beb19e237968ce884dd5019d
jonwalker <jonathan.b.walker0@gmail.com>
2015-09-27 10:29:35 -0500

    get parent comment node list

--- content.js
+++ content.js
@@ -1,10 +1,17 @@
 /* Listen for messages */
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     /* If the received message has the expected format... */
-    console.log("HAI FROM CONTENT.JS")
+    console.log("Hello from CONTENT.JS your grace")
+
+    function getParentCommentNodeList() {
+        var comments = document.querySelectorAll('[class^="entry unvoted"]' || '[class*="entry likes"]' || 'class*="entry dislikes"]');
+        console.log("comment node list", comments)
+        return comments
+    }
+
 
     if (msg.text && (msg.text == "report_back")) {
-        console.log("HAI FROM CONTENT.JS if block")
+        console.log("HAI FROM CONTENT",msg)
         /* Call the specified callback, passing 
          the web-pages DOM content as argument */
         sendResponse(document.all[0].outerHTML);
